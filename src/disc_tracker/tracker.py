e019b2eb6814148dd1451fc4b10050379091bb59
Joel Miller <joelsamiller@gmail.com>
2024-03-15 16:00:51 +0000

    refactor: Tidy `tracker.py`

--- src/disc_tracker/tracker.py
+++ src/disc_tracker/tracker.py
@@ -1,17 +1,17 @@
-from scipy.spatial import distance as dist
+from scipy.spatial import distance
 from collections import OrderedDict
 import numpy as np
 
 
 class Tracker:
-    def __init__(self, maxDisappeared=5000):
+    def __init__(self, max_disappeared=5000):
         self.nextID = 0
         self.objects = OrderedDict()
         self.disappeared = OrderedDict()
-        self.maxDisappeared = maxDisappeared
+        self.max_disappeared = max_disappeared
 
-    def register(self, pos: np.ndarray[float]):
-        self.objects[self.nextID] = [pos]
+    def register(self, position: np.ndarray[float]):
+        self.objects[self.nextID] = [position]
         self.disappeared[self.nextID] = 0
         self.nextID += 1
 
@@ -19,52 +19,54 @@ class Tracker:
         del self.objects[ID]
         del self.disappeared[ID]
 
-    def update(self, newPos: np.ndarray[float]):
-        if len(newPos) == 0:
-            for ID in list(self.disappeared.keys()):
-                self.disappeared[ID] += 1
+    def update(self, new_position: np.ndarray[float]):
+        if len(new_position) == 0:
+            for id in self.disappeared:
+                self.disappeared[id] += 1
 
-                if self.disappeared[ID] > self.maxDisappeared:
-                    self.deregister(ID)
+                if self.disappeared[id] > self.max_disappeared:
+                    self.deregister(id)
+
             return self.objects
 
         if len(self.objects) == 0:
-            for i in range(0, len(newPos)):
-                self.register(newPos[i])
+            for p in new_position:
+                self.register(p)
         else:
-            IDs = list(self.objects.keys())
-            oldPos = [positions[-1] for positions in list(self.objects.values())]
+            ids = [*self.objects]
+            old_position = np.array([p[-1] for p in self.objects.values()])
 
-            r = dist.cdist(np.array(oldPos), newPos)
+            r = distance.cdist(old_position, new_position)
 
             rows = r.min(axis=1).argsort()
             cols = r.argmin(axis=1)[rows]
 
-            usedRows = set()
-            usedCols = set()
+            used_rows = set()
+            used_cols = set()
 
             for row, col in zip(rows, cols):
-                if row in usedRows or col in usedCols:
+                if row in used_rows or col in used_cols:
                     continue
 
-                ID = IDs[row]
-                self.objects[ID].append(newPos[col])
-                self.disappeared[ID] = 0
+                id = ids[row]
+                self.objects[id].append(new_position[col])
+                self.disappeared[id] = 0
 
-                usedRows.add(row)
-                usedCols.add(col)
+                used_rows.add(row)
+                used_cols.add(col)
 
-            unusedRows = set(range(0, r.shape[0])).difference(usedRows)
-            unusedCols = set(range(0, r.shape[1])).difference(usedCols)
+            unused_rows = set(range(0, r.shape[0])).difference(used_rows)
+            unused_cols = set(range(0, r.shape[1])).difference(used_cols)
 
             if r.shape[0] >= r.shape[1]:
-                for row in unusedRows:
-                    ID = IDs[row]
-                    self.disappeared[ID] += 1
+                for row in unused_rows:
+                    id = ids[row]
+                    self.disappeared[id] += 1
 
-                    if self.disappeared[ID] > self.maxDisappeared:
-                        self.deregister(ID)
+                    if self.disappeared[id] > self.max_disappeared:
+                        self.deregister(id)
             else:
-                for col in unusedCols:
-                    self.register(newPos[col])
+                for col in unused_cols:
+                    self.register(new_position[col])
+
         return self.objects
